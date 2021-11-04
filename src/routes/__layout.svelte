bb57c16fbd342c23be379fe593e3a4861bdafb80
silvestrevivo <silvestrevivomillan@gmail.com>
2021-11-04 16:50:34 +0100

    @solana/wallet-adapter-wallets package added

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	// import {
+	// 	getLedgerWallet,
+	// 	getPhantomWallet,
+	// 	getSlopeWallet,
+	// 	getSolflareWallet,
+	// 	getSolletExtensionWallet,
+	// 	getSolletWallet,
+	// 	getTorusWallet
+	// } from '@solana/wallet-adapter-wallets';
+
+	// console.log('getPhantomWallet', getPhantomWallet);
+</script>
+
+<slot />
+
+<svelte:head>
+	<script>
+		window.global = window;
+	</script>
+</svelte:head>
